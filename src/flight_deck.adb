311b1bd6d1ee6ce57672e2a13cee46e78f91ca96
Carl Klein <carlj.klein@gmail.com>
2026-08-16 00:10:37 -0700

    pre root level vscode tree

---
+++ src/flight_deck.adb
@@ -0,0 +1,10 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Flight_Deck is
+begin
+   Put_Line ("F-16 Avionics Suite Initializing...");
+   Put_Line ("Radar................ OK");
+   Put_Line ("Navigation Systems... OK");
+   Put_Line ("Weapons Systems....... OK");
+   Put_Line ("Power-On Self-Test Complete. Ready for flight.");
+end Flight_Deck;
