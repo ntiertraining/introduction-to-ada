9ca76167e7164c5f13a81540823e30151eb2c83e
Joel Mussman <jmussman@smallrock.net>
2026-08-23 08:41:25 -0400

    Adding scripts for Google Cloud Shell

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,26 +1,27 @@
-# Cloud Shell Setup Script
+# Google Cloud Shell Software Installation
+#   - CS always provides x86_64 Debian Linux environment
+#   - Software installation is not preserved, only the file system. This script must be run on every boot.
+#   - CS has no mechanism to launch a script automatically, manual interventions is required.
+#   - This script must be run with system administrator privileges
 #
 
 # Install system dependencies required for building and unpacking Ada tooling
 
 apt-get update
-apt-get install -y --no-install-recommends \
-    build-essential \
-    curl \
-    jq \
-    unzip \
-    gnat \
-    gprbuild \
-    ca-certificates
-rm -rf /var/lib/apt/lists/*
+apt-get install -y --no-install-recommends build-essential gnat gprbuild
 
 # Install the correct Alire package for this environment
 
 alr_latest=$(curl -sL https://api.github.com/repos/alire-project/alire/releases/latest | jq -r .tag_name)
-curl -L -o vscode.deb "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
-dpkg -i vscode.deb || apt-get install -y --fix-broken
-rm vscode.deb
 curl -sLO "https://github.com/alire-project/alire/releases/download/${alr_latest}/alr-${alr_latest#v}-bin-x86_64-linux.zip"
 unzip alr-${alr_latest#v}-bin-x86_64-linux.zip
 mv ./bin/alr /usr/local/bin/alr
 rm -rf ./bin
+
+# Add VS Code extensions (if they are not alreay there)
+
+/google/devshell/editor/code-oss-for-cloud-shell/bin/codeoss-cloudshell --install-extension adacore.ada
+
+# Kill the actual Google CS window
+
+# pkill -9 -f -- '^-bash$'
